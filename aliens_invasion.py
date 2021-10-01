4e91a8ff778188c0acb58672b7c0f57b619e590c
Tsuneki Akatsu <tsunekiakatsu@Tsunekis-MacBook-Pro.local>
2021-10-01 21:21:56 +0100

    add images to the game

---
+++ aliens_invasion.py
@@ -0,0 +1,38 @@
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+class AliensInvasion:
+    """Overall class to manage game assets & behaviour."""
+
+    def __init__(self):
+        """Initialize the game & create game resources."""
+        pygame.init()
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien's Invasion")
+
+        self.ship = Ship(self)
+
+    
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    ai = AliensInvasion()
+    ai.run_game()
